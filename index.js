4da0b98f2c4ed84ae7a0077dd562e1e9357ff815
jrkassis <joerkassis@gmail.com>
2023-03-31 11:13:59 +0300

    Added Express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var express = require('express')
 var cors = require('cors')
 app.use(cors())
+var app = express()
 
 fetch("https://jrkapitest.onrender.com/")
   .then((response) => response.json())
   .then((data) => console.log(data));
+
